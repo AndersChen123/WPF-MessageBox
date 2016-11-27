ea4232af61ee786ef5fabb8679b2a476d3fa8c12
Anders chan <419790899@qq.com>
2016-11-27 15:10:18 +0800

    First commit.

---
+++ WPFMessageBoxControl.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Automation.Peers;
+
+namespace MessageBoxUtils
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:WPFMessageBox"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:WPFMessageBox;assembly=WPFMessageBox"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    [TemplatePart(Name = "PART_YesButton", Type = typeof(Button))]
+    [TemplatePart(Name = "PART_NoButton", Type = typeof(Button))]
+    [TemplatePart(Name = "PART_OkButton", Type = typeof(Button))]
+    [TemplatePart(Name = "PART_CancelButton", Type = typeof(Button))]
+    public class WPFMessageBoxControl : Control
+    {
+        static WPFMessageBoxControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WPFMessageBoxControl), new FrameworkPropertyMetadata(typeof(WPFMessageBoxControl)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            YesButton = base.GetTemplateChild("PART_YesButton") as Button;
+            NoButton = base.GetTemplateChild("PART_NoButton") as Button;
+            OkButton = base.GetTemplateChild("PART_OkButton") as Button;
+            CancelButton = base.GetTemplateChild("PART_CancelButton") as Button;    
+        }
+
+        internal Button YesButton { get; set; }
+        internal Button NoButton { get; set; }
+        internal Button OkButton { get; set; }
+        internal Button CancelButton { get; set; }
+
+        protected override System.Windows.Automation.Peers.AutomationPeer OnCreateAutomationPeer()
+        {
+            return new WPFMessageBoxControlAutomationPeer(this);
+        }
+    }
+}
---
+++ WPFMessageBoxControlAutomationPeer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Automation.Peers;
+using System.Windows;
+
+namespace MessageBoxUtils
+{
+    class WPFMessageBoxControlAutomationPeer : UIElementAutomationPeer
+    {
+        public WPFMessageBoxControlAutomationPeer( WPFMessageBoxControl owner) : base(owner)
+        {
+        }
+
+        protected override string GetClassNameCore()
+        {
+            return "WPFMessageBoxControl";
+        }
+    }
+}
